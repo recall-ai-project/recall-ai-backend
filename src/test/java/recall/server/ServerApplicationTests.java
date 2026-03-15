fbf79206fd8925d7e11d871f51db5e07e92247ca
tomato716 <pjh203203@naver.com>
2026-03-15 18:30:57 +0900

    Initial commit

---
+++ src/test/java/recall/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package recall.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
